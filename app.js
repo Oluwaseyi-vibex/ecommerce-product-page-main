45fc34e09a9565db37a8b1d22aaeee6cce405bb9
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2023-08-30 16:26:28 +0100

    cart update

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ $(document).ready(function () {
 //COUNTER
 $(document).ready(function () {
   let counterValue = 0;
-
+  const filledCart = $(".filled-cart");
+  const emptycart = $(".emptycart");
   function updateCounter() {
     return parseInt($("#num").text(counterValue), 10);
     // $("#num").text(counterValue);
@@ -22,16 +23,35 @@ $(document).ready(function () {
   $("#minus").click(function () {
     counterValue -= 1;
     updateCounter();
+    // updateCounter();
   });
 
   $("#plus").click(function () {
     counterValue += 1;
     updateCounter();
   });
-});
 
-//filler
-$(document).ready(function () {
-  const filledCart = $(".filled-cart");
-  const emptycart = $(".emptycart");
+  $("#add-to-cart-button").click(function () {
+    $(".cart-cnt").toggle();
+    if (counterValue === 0) {
+      updateCounter();
+
+      filledCart.remove();
+      emptycart.css("display", "flex");
+    } else {
+      filledCart.css("display", "flex");
+      emptycart.remove();
+    }
+    if (counterValue > 0) {
+      updateCounter();
+
+      filledCart.css("display", "flex");
+      emptycart.remove();
+    } else {
+      updateCounter();
+
+      filledCart.remove();
+      emptycart.css("display", "flex");
+    }
+  });
 });
